88adee47e50554dd3a7eddfb5bbe169e20e293e1
Luiz Henrique <luizhjramos@outlook.com>
2021-09-09 19:45:43 -0300

    refactor: reinstall dependencies

--- src/components/pomodoro-timer.tsx
+++ src/components/pomodoro-timer.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { useEffect } from "react";
+import React, { useEffect } from "react";
+
 import { useInterval } from "../hooks/use-interval";
 import { Button } from "./button";
 import { Timer } from "./timer";
